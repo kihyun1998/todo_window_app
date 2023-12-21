956aabd6e1908b4d090130edf05d83184b6e1bcd
wantSome <kihyun1998@naver.com>
2023-12-21 20:13:48 +0900

    consumer 위젯으로 리팩토링

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,32 @@ void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // 화면에 디버그 줄 안나오게 하기
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (BuildContext context, WidgetRef ref, Widget? child) =>
+          MaterialApp(
+        debugShowCheckedModeBanner: false, // 화면에 디버그 줄 안나오게 하기
 
-      /// local 설정
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      locale: ref.watch(langProvider),
+        /// local 설정
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        locale: ref.watch(langProvider),
 
-      /// theme 설정
-      theme: ref.themeData,
+        /// theme 설정
+        theme: ref.themeData,
 
-      /// homepage 지정
-      home: const HomePage(),
+        /// homepage 지정
+        home: const HomePage(),
+      ),
     );
   }
 }
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -4,20 +4,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_window_app/src/service/theme_riverpod.dart';
 import 'package:todo_window_app/util/lang/generated/l10n.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends StatelessWidget {
   const HomePage({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: ref.watch(themeProvider.notifier).toggleTheme,
-          child: Text(
-            S.current.theme,
-            style: ref.font.headline6,
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (BuildContext context, WidgetRef ref, Widget? child) => Scaffold(
+        body: Center(
+          child: TextButton(
+            onPressed: ref.watch(themeProvider.notifier).toggleTheme,
+            child: Text(
+              S.current.theme,
+              style: ref.font.headline6,
+            ),
           ),
         ),
       ),

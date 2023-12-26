b4cea7c04901f059f9dbf44a55a31e1f17d3acbd
wantSome <kihyun1998@naver.com>
2023-12-26 17:18:26 +0900

    listview 안된다

--- lib/src/view/setting/setting_view.dart
+++ lib/src/view/setting/setting_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_window_app/src/view/setting/widgets/settingNavigationBar.dart';
+import 'package:todo_window_app/src/view/setting/widgets/settingTile.dart';
 import 'package:todo_window_app/src/view/widgets/bodyWidget.dart';
 import 'package:todo_window_app/src/view/widgets/mainWidget.dart';
 import 'package:todo_window_app/util/lang/generated/l10n.dart';
@@ -9,13 +10,37 @@ class SettingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<SettingTile> globalConfigList = [
+      SettingTile(
+        icon: 'theme',
+        title: "Theme",
+        subtitle: "Theme",
+        onPressed: () {},
+      ),
+      SettingTile(
+        icon: 'theme',
+        title: "Theme",
+        subtitle: "Theme",
+        onPressed: () {},
+      ),
+    ];
     return Scaffold(
-        appBar: AppBar(title: Text(S.current.setting)),
-        body: const BodyWidget(
-          navigationBar: SettingNavigationBar(),
-          mainPage: MainWidget(
-            child: Text("test"),
+      appBar: AppBar(title: Text(S.current.setting)),
+      body: BodyWidget(
+        navigationBar: const SettingNavigationBar(),
+        mainPage: MainWidget(
+          child: Column(
+            children: [
+              ListView.builder(
+                itemCount: globalConfigList.length,
+                itemBuilder: (context, index) {
+                  return globalConfigList[index];
+                },
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
